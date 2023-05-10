330f9773d72524f79f70ff635d2d48317637fb5f
Derek Woodard <woodard.derek92@gmail.com>
2023-05-10 17:15:32 -0300

    exporting map data to layers

--- code/editor.py
+++ code/editor.py
@@ -63,8 +63,8 @@ class Editor:
 			group = self.canvas_objects)
 
 	# support
-	def get_current_cell(self):
-		distance_to_origin = vector(mouse_pos()) - self.origin
+	def get_current_cell(self, obj = None):
+		distance_to_origin = vector(mouse_pos()) - self.origin if not obj else vector(obj.distance_to_origin) - self.origin
 
 		if distance_to_origin.x > 0:
 			col = int(distance_to_origin.x / TILE_SIZE)
@@ -131,12 +131,73 @@ class Editor:
 			if sprite.rect.collidepoint(mouse_pos()):
 				return sprite
 	
+	def create_grid(self):
+		
+		# add objects to tiles
+		for tile in self.canvas_data.values():
+			tile.objects = []
+
+		for obj in self.canvas_objects:
+			current_cell = self.get_current_cell(obj)
+			offset = vector(obj.distance_to_origin) - (vector(current_cell) * TILE_SIZE)
+
+			if current_cell in self.canvas_data: # tile exists already
+				self.canvas_data[current_cell].add_id(obj.tile_id, offset)
+			else: # no tile exists yet
+				self.canvas_data[current_cell] = CanvasTile(obj.tile_id, offset)
+		
+		# create an empty grid
+		layers = {
+			'water': {},
+			'bg palms': {},
+			'terrain': {},
+			'enemies': {},
+			'coins': {},
+			'fg objects': {}
+		}
+
+		# grid offset
+		left = sorted(self.canvas_data.keys(), key = lambda tile: tile[0])[0][0]
+		top = sorted(self.canvas_data.keys(), key = lambda tile: tile[1])[0][1]
+
+		# fill the grid
+		for tile_pos, tile in self.canvas_data.items():
+			row_adjusted = tile_pos[1] - top
+			col_adjusted = tile_pos[0] - left
+			x = col_adjusted * TILE_SIZE
+			y = row_adjusted * TILE_SIZE
+
+			if tile.has_water:
+				layers['water'][(x,y)] = tile.get_water()
+
+			if tile.has_terrain:
+				layers['terrain'][(x,y)] = tile.get_terrain() if tile.get_terrain() in self.land_tiles else 'X'
+
+			if tile.coin:
+				layers['coin'][(x + TILE_SIZE // 2,y + TILE_SIZE // 2)] = tile.coin
+
+			if tile.enemy:
+				layers['enemies'][(x,y)] = tile.enemy
+
+			if tile.objects:
+				for obj, offset in tile.objects:
+					if obj in [key for key, value in EDITOR_DATA.items() if value['style'] == 'palm_bg']:
+						layers['bg palms'][(int(x + offset.x), int(y + offset.y))] = obj
+					else:
+						layers['fg objects'][(int(x + offset.x), int(y + offset.y))] = obj
+
+		return layers
+
 	# input
 	def event_loop(self):
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 				sys.exit()
+			if event.type == pygame.KEYDOWN and event.key == pygame.K_RETURN:
+				print(self.create_grid())
+
+
 			self.pan_input(event)
 			self.selection_hotkeys(event)
 			self.menu_click(event)
@@ -402,7 +463,7 @@ class Editor:
 		self.menu.display(self.selection_index)
 
 class CanvasTile:
-	def __init__(self, tile_id):
+	def __init__(self, tile_id, offset = vector()):
 
 		# terrain
 		self.has_terrain = False
@@ -421,16 +482,19 @@ class CanvasTile:
 		# objects
 		self.objects = []
 
-		self.add_id(tile_id)
+		self.add_id(tile_id, offset = offset)
 		self.is_empty = False
 
-	def add_id(self, tile_id):
+	def add_id(self, tile_id, offset = vector()):
 		options = {key: value['style'] for key, value in EDITOR_DATA.items()}
 		match options[tile_id]:
 			case 'terrain': self.has_terrain = True
 			case 'water': self.has_water = True
 			case 'coin': self.coin = tile_id
 			case 'enemy': self.enemy = tile_id
+			case _: # objects
+				if (tile_id, offset) not in self.objects:
+					self.objects.append((tile_id, offset))
 
 	def remove_id(self, tile_id):
 		options = {key: value['style'] for key, value in EDITOR_DATA.items()}
@@ -444,6 +508,16 @@ class CanvasTile:
 	def check_content(self):
 		if not self.has_terrain and not self.has_water and not self.coin and not self.enemy:
 			self.is_empty = True
+
+	def get_water(self):
+		return 'bottom' if self.water_on_top else 'top'
+
+	def get_terrain(self):
+		return ''.join(self.terrain_neighbours)
+
+
+
+
 
 class CanvasObject(pygame.sprite.Sprite):
 	def __init__(self, pos, frames, tile_id, origin, group):
